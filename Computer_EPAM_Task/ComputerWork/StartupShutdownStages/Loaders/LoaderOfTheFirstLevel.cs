19bfbb1693108196a5cbad590b0d20e8c2d076d8
KotKat <kotkat.lv@gmail.com>
2020-07-04 23:01:41 +0300

    v 1.0.0

---
+++ Computer_EPAM_Task/ComputerWork/StartupShutdownStages/Loaders/LoaderOfTheFirstLevel.cs
@@ -0,0 +1,16 @@
+﻿using Computer_EPAM_Task.Interfaces;
+
+namespace Computer_EPAM_Task.ComputerWork.StartupShutdownStages.Loaders
+{
+    // Загрузчик 1-го уровня. Master Boot Record
+    internal class LoaderOfTheFirstLevel : IPCState
+    {
+        public bool TurnOff() => true;
+
+        // Если был успешно пройден этап самотестирования это означает, что процессы первого этапа заввершены успешно и мы переходим дальше
+        public bool TurnOn() => POST();
+
+        // Самотестирование: проверяем аппаратные сбои, один сигнал после POST сигнализирует, что все в порядке.
+        private bool POST() => true;
+    }
+}
