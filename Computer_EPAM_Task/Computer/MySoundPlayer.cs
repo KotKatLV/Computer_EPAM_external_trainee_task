46234fed2638aaf1c262bf86004994527a798125
KotKat <kotkat.lv@gmail.com>
2020-07-04 20:47:47 +0300

    bla bla bla

---
+++ Computer_EPAM_Task/Computer/MySoundPlayer.cs
@@ -0,0 +1,69 @@
+﻿using Computer_EPAM_Task.Interfaces;
+using System;
+using System.IO;
+using System.Media;
+using System.Windows;
+
+namespace Computer_EPAM_Task.Computer
+{
+    internal class MySoundPlayer : ISoundPlayer
+    {
+        private SoundPlayer sp;
+
+        public void PlayLoadSound()
+        {
+            try
+            {
+                using (SoundPlayer sp = new SoundPlayer())
+                {
+                    sp.Stream = Properties.Resources.loadMus;
+                    sp.Play();
+                }
+            }
+            catch (TimeoutException te)
+            {
+                MessageBox.Show(te.Message);
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                MessageBox.Show(fnfe.Message);
+            }
+            catch (InvalidOperationException ioe)
+            {
+                MessageBox.Show(ioe.Message);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        public void PlayShutDownSound()
+        {
+            try
+            {
+                using (sp = new SoundPlayer())
+                {
+                    sp.Stream = Properties.Resources.shutDownMus;
+                    sp.Play();
+                }
+            }
+            catch (TimeoutException te)
+            {
+                MessageBox.Show(te.Message);
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                MessageBox.Show(fnfe.Message);
+            }
+            catch (InvalidOperationException ioe)
+            {
+                MessageBox.Show(ioe.Message);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+    }
+}
---
+++ Computer_EPAM_Task/Interfaces/ISoundPlayer.cs
@@ -0,0 +1,15 @@
+﻿namespace Computer_EPAM_Task.Interfaces
+{
+    internal interface ISoundPlayer
+    {
+        /// <summary>
+        /// Проигрвание музыки включения ОС
+        /// </summary>
+        void PlayLoadSound();
+
+        /// <summary>
+        /// Проигрвание музыки выключения ОС
+        /// </summary>
+        void PlayShutDownSound();
+    }
+}
